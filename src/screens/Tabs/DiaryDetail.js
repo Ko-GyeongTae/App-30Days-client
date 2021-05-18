4e6ac9314e296e38c6aea432e5b79a95a9adef28
Ko-GyeongTae <kokt0203@naver.com>
2021-05-18 23:41:43 +0900

    <EDIT> DiaryDetail

--- src/screens/Tabs/DiaryDetail.js
+++ src/screens/Tabs/DiaryDetail.js
@@ -10,10 +10,13 @@ const Header = styled.View`
   align-items: center;
 `;
 
-const Body = styled.View`
-  height: 87%;
-  width: 100%;
-  backgroundColor: #f5f5f5;
+const TextBox = styled.View`
+    width: 340px;
+    height: 500px;
+    margin-top: 30px;
+    border-radius: 10px;
+    elevation: 10;
+    background-color: #ffffff;
 `;
 
 const Title = styled.Text`
@@ -24,22 +27,23 @@ const Title = styled.Text`
 const AllView = styled.View`
   flex: 1;
   height: 100%;
+  align-items: center;
+  justify-content: flex-start;
+  background-color: #f1f1f1;
 `;
 
 export default ({ route }) => {
-    const post = route.params;
-    const date = new Date(post.date);
-    return (
-        <AllView>
-            <ImageBackground source={require("../../../assets/coffee.png")} style={{ width: "100%", height: "100%", alignItems: "center" }}>
-                <Header>
-                    <Title>{post.title}</Title>
-                    <Text>{`${date.getFullYear()}년 ${date.getMonth() + 1}월 ${date.getDate() - 1 < 10 ? "0" + date.getDate() - 1 : date.getDate()}일`}</Text>
-                </Header>
-                <Body>
-                    <Text>{post.content}</Text>
-                </Body>
-            </ImageBackground>
-        </AllView>
-    );
+  const post = route.params;
+  const date = new Date(post.date);
+  return (
+    <AllView>
+      <Header>
+        <Title>{post.title}</Title>
+        <Text>{`${date.getFullYear()}년 ${date.getMonth() + 1}월 ${date.getDate() - 1 < 10 ? "0" + date.getDate() - 1 : date.getDate()}일`}</Text>
+      </Header>
+      <TextBox>
+        <Text style={{padding: 10}}>{post.content}</Text>
+      </TextBox>
+    </AllView>
+  );
 };
